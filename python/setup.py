ceb70d856bfc81a7a7d2502039f3e572fd9bc477
Thomas Nicollet <thomasnicollet@yahoo.fr>
2018-09-06 21:40:17 +0200

    Bumped version to v0.0.7

--- python/setup.py
+++ python/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="graphcall",
-    version="0.0.6",
+    version="0.0.7",
     author="nwmqpa",
     author_email="thomas.nicollet@epitech.eu",
     description="A lightweight graphQL library",
