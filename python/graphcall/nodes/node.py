3dc9e19ebbdc9f6ecbc43e68b989963b4cb37e5b
Thomas Nicollet <thomasnicollet@yahoo.fr>
2018-09-07 22:56:36 +0200

    Hopefully fixing the bug this time

--- python/graphcall/nodes/node.py
+++ python/graphcall/nodes/node.py
@@ -23,7 +23,8 @@ class Node(Compilable):
         """Add a node to the current node."""
         if self.nodes is None:
             self.nodes = []
-        node = Node("")._init(name, self, self.nesting + "\t")
+        node = Node("")
+        node._init(name, self, self.nesting + "\t")
         self.nodes.append(
             node
         )
